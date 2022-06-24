628064bf3b2bf07285b2c3129b924ec31a35cb7c
jessgschueler <jessicagrace.schueler@gmail.com>
2022-06-24 11:05:39 -0700

    Cast artist_popularity and followers as int

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
+from pyspark.sql.types import IntegerType
 
 #create instance of SparkSession
 spark = SparkSession.builder.getOrCreate()
@@ -18,6 +19,9 @@ spark_df.select(spark_df.name, spark_df.genres).show(10)
 
 #clean data
 
-#filter for genres column and use fillna to replace
+#create udf to replace empty brackets in genres col
 replace = udf(lambda x: "['elevator music']" if x == "[]" else x)
 spark_df.select(replace(spark_df.genres)).show(10)
+#cast artist_popularity and followers as int
+spark_df = spark_df.withColumn('artist_popularity', spark_df['artist_popularity'].cast(IntegerType()))
+spark_df = spark_df.withColumn('followers', spark_df['followers'].cast(IntegerType()))
